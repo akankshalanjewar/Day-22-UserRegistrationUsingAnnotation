39d342c30ab825f686fd9f01b06967ad9b663f87
akankshalanjewar <100331821+akankshalanjewar@users.noreply.github.com>
2022-04-26 12:39:12 +0530

    Update Annoation.cs

--- UserRegistrationUsingAnnotation/Annoation.cs
+++ UserRegistrationUsingAnnotation/Annoation.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -36,7 +36,7 @@ namespace UserRegistrationUsingAnnotation
                 Console.WriteLine("FirstName: " + userRegistration.FirstName1);
             }
 
-            Console.ReadKey();
+            
         }
     }
 
